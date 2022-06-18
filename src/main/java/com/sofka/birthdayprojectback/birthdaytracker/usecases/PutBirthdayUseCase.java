180bb1b59a7049e049ab59d3dc72f93a23a6069b
David <depch47@gmail.com>
2022-06-18 14:41:56 -0500

    Put usecase updated. Now compares secrets before updating

--- src/main/java/com/sofka/birthdayprojectback/birthdaytracker/usecases/PutBirthdayUseCase.java
+++ src/main/java/com/sofka/birthdayprojectback/birthdaytracker/usecases/PutBirthdayUseCase.java
@@ -1,5 +1,6 @@
 package com.sofka.birthdayprojectback.birthdaytracker.usecases;
 
+import com.sofka.birthdayprojectback.birthdaytracker.bcrypt.BcryptMapper;
 import com.sofka.birthdayprojectback.birthdaytracker.dto.BirthdayDTO;
 import com.sofka.birthdayprojectback.birthdaytracker.mapper.BirthdayMapper;
 import com.sofka.birthdayprojectback.birthdaytracker.repository.BirthdayRepository;
@@ -15,19 +16,30 @@ public class PutBirthdayUseCase {
     private final BirthdayMapper mapper;
     private final BirthdayRepository repository;
     private final DateFormatter dateFormatter;
+    private final BcryptMapper bcrypt;
 
 
     public static void main(String[] args) {
     }
-    public PutBirthdayUseCase(BirthdayMapper mapper, BirthdayRepository repository, DateFormatter dateFormatter) {
+    public PutBirthdayUseCase(BirthdayMapper mapper, BirthdayRepository repository, DateFormatter dateFormatter, BcryptMapper bcrypt) {
         this.mapper = mapper;
         this.repository = repository;
         this.dateFormatter = dateFormatter;
+        this.bcrypt = bcrypt;
     }
 
     public Mono<BirthdayDTO> apply(@Valid BirthdayDTO dto) {
-        dateFormatter.setUserAgeFromDate(dto);
-        return repository.save(mapper.dtoToEntity(dto)).map(mapper::entityToDTO);
+        return repository.findById(dto.getId())
+                .switchIfEmpty(Mono.error(() -> new IllegalStateException("Birthday not found " + dto.getId())))
+                .filter(entity -> bcrypt.compare(dto.getSecret(), entity.getSecret()))
+                .switchIfEmpty(Mono.error(() -> new IllegalStateException("Birthday's secret do not match " + dto.getId())))
+                .flatMap(entity -> {
+                    dateFormatter.setUserAgeFromDate(dto);
+                    dto.setSecret(entity.getSecret());
+                    return repository.save(mapper.dtoToEntity(dto));
+                })
+                .map(mapper::entityToDTO);
+
     }
 
 
